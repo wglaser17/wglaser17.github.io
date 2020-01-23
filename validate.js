bbab52c6d14fa6328e27ccbf3db2941d71d7430f
wglaser <william_glaser@brown.edu>
2020-01-23 17:00:19 -0500

    Initial commit

---
+++ validate.js
@@ -0,0 +1,30 @@
+function show_image(src) {
+    var img = document.createElement("img");
+    img.src = src;
+
+    // This next line will just add it to the <body> tag
+    document.body.appendChild(img);
+}
+
+function check_code(){
+    var parameters = location.search.substring(1).split("&");
+    let map = {};
+    for (let k in parameters){
+        let temp = k.split("=");
+        map[temp[0]] = temp[1]
+    }
+    return map['k'];
+}
+
+    function toPython(usrdata){
+        $.ajax({
+            url: "http://localhost:8000",
+            type: "POST",
+            data: { information : "You have a very nice website, sir." , userdata : usrdata},
+            dataType: "json",
+            success: function(data) {
+                <!-- do something here -->
+                console.log(data);
+                $('#test').html(data);
+            }});
+}
